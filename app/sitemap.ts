5afbe6fe0898afca2b530de356ea400c80166b66
Maurice Pennington Bey <islamismylifebey@gmail.com>
2026-09-07 10:54:48 -0500

    fix: make sitemap route static

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { routes, site } from "./content";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: site.url, changeFrequency: "monthly", priority: 1 },
